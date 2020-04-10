ff04a47ca401172253151602f9bd70cab1126054
shubham.pol <shubham.pol@zeuslearning.com>
2020-04-10 20:50:01 +0530

    code with camera on :D

--- Spol/start.js
+++ Spol/start.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express();
 var path = require('path');
+var https = require('https')
+var fs = require('fs')
 const port = 3000
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
-app.use('/static', express.static(path.join(__dirname, '/static')))
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.use('/static', express.static(path.join(__dirname, '/static')));
+
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+.listen(3000, function () {
+  console.log('Example app listening on port 3000! Go to https://localhost:3000/')
+})
